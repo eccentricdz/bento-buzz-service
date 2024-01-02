3d98801967229677ede364ef76e83b85b996dd8a
Rahul Agarwal <rahul.agarwal@cloudkitchens.com>
2024-01-01 18:41:43 -0800

    Add cors.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { fetchArticles } from "./utils/data";
 import { andThen, pipe } from "ramda";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.get(
   "/",
   // eslint-disable-next-line
